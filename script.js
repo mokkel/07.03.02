a1d4712add4d005af79ea3c200b1fa65e7dd2f81
cirk0391 <cirk0391@stud.kea.dk>
2022-02-18 10:01:57 +0100

    API key

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
+
+// NYT
+
+const url = "https://planter-5959.restdb.io/rest/_swagger.json";
+const key = "620f56fb34fd621565858796";
+
+const options = {
+  headers: {
+    "x-apikey": "620f56fb34fd621565858796",
+  },
+};
