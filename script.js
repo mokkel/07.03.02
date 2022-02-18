358d07bec2c10eade3e5d33aa0f264413f44ad5c
mokkel <61493027+mokkel@users.noreply.github.com>
2022-02-18 11:45:12 +0100

    header and css changes

--- script.js
+++ script.js
@@ -28,70 +28,70 @@ const options = {
 
 */
 
-const url = "https://planter-5959.restdb.io/rest/_swagger.json";
-const key = "620f56fb34fd621565858796";
-let menu;
-let filter = "alle";
-const filtrerKnap = document.querySelectorAll("button");
+// const url = "https://planter-5959.restdb.io/rest/_swagger.json";
+// const key = "620f56fb34fd621565858796";
+// let menu;
+// let filter = "alle";
+// const filtrerKnap = document.querySelectorAll("button");
 
-const options = {
-  headers: {
-    "x-apikey": key,
-  },
-};
+// const options = {
+//   headers: {
+//     "x-apikey": key,
+//   },
+// };
 
-document.addEventListener("DOMContentLoaded", start);
+// document.addEventListener("DOMContentLoaded", start);
 
-function start() {
-  filtrerKnap.forEach((knap) =>
-    knap.addEventListener("click", filtrerKategori)
-  );
-  hentdata();
-}
+// function start() {
+//   filtrerKnap.forEach((knap) =>
+//     knap.addEventListener("click", filtrerKategori)
+//   );
+//   hentdata();
+// }
 
-function filtrerKategori() {
-  filter = this.dataset.menu;
-  console.log("filter", filter);
-  document.querySelector("h1").textContent = this.textContent;
+// function filtrerKategori() {
+//   filter = this.dataset.menu;
+//   console.log("filter", filter);
+//   document.querySelector("h1").textContent = this.textContent;
 
-  vis();
-}
+//   vis();
+// }
 
-async function hentdata() {
-  console.log("her er json");
-  const respons = await fetch(url, options);
-  menu = await respons.json();
-  vis();
-}
+// async function hentdata() {
+//   console.log("her er json");
+//   const respons = await fetch(url, options);
+//   menu = await respons.json();
+//   vis();
+// }
 
-const container = document.querySelector("section");
-const temp = document.querySelector("template");
+// const container = document.querySelector("section");
+// const temp = document.querySelector("template");
 
-function vis() {
-  console.log(menu);
-  container.innerHTML = "";
-  menu.forEach((ret) => {
-    if (filter == ret.kategori || filter == "alle") {
-      /*____klon er altså alt inde i template 'temp'_____*/
-      const klon = temp.cloneNode(true).content;
-      klon.querySelector("h2").textContent = ret.navn;
-      klon.querySelector("h3").textContent = ret.pris + ".kr";
-      klon.querySelector("p").textContent = ret.kortbeskrivelse;
-      klon.querySelector("img").src = `medium/${ret.billednavn}-md.jpg`;
-      /*___kald til at åbne i ny side_____*/
-      klon.querySelector("article").addEventListener("click", () => {
-        location.href = `single_view.html?id=${ret._id}`;
-      });
+// function vis() {
+//   console.log(menu);
+//   container.innerHTML = "";
+//   menu.forEach((ret) => {
+//     if (filter == ret.kategori || filter == "alle") {
+//       /*____klon er altså alt inde i template 'temp'_____*/
+//       const klon = temp.cloneNode(true).content;
+//       klon.querySelector("h2").textContent = ret.navn;
+//       klon.querySelector("h3").textContent = ret.pris + ".kr";
+//       klon.querySelector("p").textContent = ret.kortbeskrivelse;
+//       klon.querySelector("img").src = `medium/${ret.billednavn}-md.jpg`;
+//       /*___kald til at åbne i ny side_____*/
+//       klon.querySelector("article").addEventListener("click", () => {
+//         location.href = `single_view.html?id=${ret._id}`;
+//       });
 
-      container.appendChild(klon);
-    }
-  });
-  function visDetaljer(kategori) {
-    console.log(kategori);
-    modal.querySelector("h2").textContent = kategori.navn;
-    modal.querySelector("h3").textContent = kategori.pris;
-    modal.querySelector("p").textContent = kategori.kortbeskrivelse;
-    modal.querySelector("p").textContent = kategori.langbeskrivelse;
-    modal.style.display = "block";
-  }
-}
+//       container.appendChild(klon);
+//     }
+//   });
+//   function visDetaljer(kategori) {
+//     console.log(kategori);
+//     modal.querySelector("h2").textContent = kategori.navn;
+//     modal.querySelector("h3").textContent = kategori.pris;
+//     modal.querySelector("p").textContent = kategori.kortbeskrivelse;
+//     modal.querySelector("p").textContent = kategori.langbeskrivelse;
+//     modal.style.display = "block";
+//   }
+// }
